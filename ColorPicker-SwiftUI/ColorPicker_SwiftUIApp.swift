bf6befad14f1af95846dde9e32cee3ede9d7df9a
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 18:05:43 -0700

    Initial Commit

---
+++ ColorPicker-SwiftUI/ColorPicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPicker_SwiftUIApp.swift
+//  ColorPicker-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorPicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
