5d7a194e799664a84afe4c295de70da59ac16cde
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 18:30:24 -0700

    ColorPicker, DatePicker, TextEditor, Counter

--- ColorPicker-SwiftUI/ContentView.swift
+++ ColorPicker-SwiftUI/ContentView.swift
@@ -7,10 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
+// MARK: - Counter
+class Counter: ObservableObject {
+    @Published var value: Int = 0
+}
+
+struct CounterView: View {
+    @ObservedObject var counter = Counter()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(counter.value)")
+            Button {
+                counter.value += 3
+            } label: {
+                Text("Counter")
+                    .foregroundColor(.white)
+            }.padding()
+                .background(.blue)
+                .cornerRadius(20)
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    // MARK: - ColorPicker
+    @State private var color: Color = .white
+    
+    // MARK: - DatePicker
+    @State private var selectedDate = Date()
+    
+    // MARK: - TextEditor
+    @State private var description: String = ""
+    
+    var body: some View {
+        VStack {
+            // MARK: - ColorPicker
+            ColorPicker("Select a color", selection: self.$color)
+                .padding()
+            
+            // MARK: - DatePicker
+            DatePicker("Select date", selection: $selectedDate)
+                .padding()
+            
+            // MARK: - TextEditor
+            TextEditor(text: $description)
+                .padding()
+                .background(.green)
+                .cornerRadius(30)
+                .frame(width: 300, height: 300)
+            
+            // MARK: - Counter
+            VStack {
+                Text("Counter View")
+                CounterView()
+            }.padding()
+            .background(.yellow)
+            .cornerRadius(10)
+        }
     }
 }
 
